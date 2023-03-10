34a449f9b7347d09dc5b447858bd3bacefb660ca
GamerNoTitle <GamerNoTitle@outlook.com>
2023-03-10 18:35:12 +0800

    I forget to change link parser

--- utils/linkParse.py
+++ utils/linkParse.py
@@ -8,159 +8,159 @@ def getLink(raw_message):
     noParse = False
     url = config['api']['address']
     match raw_message:  # 匹配规则，不满足的写在后面（要解析的那些）
-        case '/警报':
+        case '!!警报':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['alerts']
-        case '/alerts':
+        case '!!alerts':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['alerts']
-        case '/新闻':
+        case '!!新闻':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['news']
-        case '/news':
+        case '!!news':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['news']
-        case '/活动':
+        case '!!活动':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['events']
-        case '/events':
+        case '!!events':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['events']
-        case '/突击':
+        case '!!突击':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['sortie']
-        case '/sortie':
+        case '!!sortie':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['sortie']
-        case '/地球赏金':
+        case '!!地球赏金':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['ostrons']
-        case '/ostrons':
+        case '!!ostrons':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['ostrons']
-        case '/cetus':
+        case '!!cetus':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['ostrons']
-        case '/金星赏金':
+        case '!!金星赏金':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['solaris']
-        case '/solaris':
+        case '!!solaris':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['solaris']
-        case '/火卫二赏金':
+        case '!!火卫二赏金':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['entratiSyndicate']
-        case '/entrati':
+        case '!!entrati':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['entratiSyndicate']
-        case '/裂缝':
+        case '!!裂缝':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['fissures']
-        case '/虚空裂缝':
+        case '!!虚空裂缝':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['fissures']
-        case '/fissures':
+        case '!!fissures':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['fissures']
-        case '/入侵':
+        case '!!入侵':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['invasions']
-        case '/invasions':
+        case '!!invasions':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['invasions']
-        case '/奸商':
+        case '!!奸商':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['voidTrader']
-        case '/虚空商人':
+        case '!!虚空商人':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['voidTrader']
-        case '/voidtrader':
+        case '!!voidtrader':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['voidTrader']
-        case '/每日特惠':
+        case '!!每日特惠':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['dailyDeals']
-        case '/特惠':
+        case '!!特惠':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['dailyDeals']
-        case '/darvo':
+        case '!!darvo':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['dailyDeals']
-        case '/dailydeals':
+        case '!!dailydeals':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['dailyDeals']
-        case '/地球时间':
+        case '!!地球时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cetusCycle']
-        case '/地球平原时间':
+        case '!!地球平原时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cetusCycle']
-        case '/希图斯时间':
+        case '!!希图斯时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cetusCycle']
-        case '/cetuscycle':
+        case '!!cetuscycle':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cetusCycle']
-        case '/舰队':
+        case '!!舰队':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['constructionProgress']
-        case '/construction':
+        case '!!construction':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['constructionProgress']
-        case '/金星时间':
+        case '!!金星时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['vallisCycle']
-        case '/金星平原时间':
+        case '!!金星平原时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['vallisCycle']            
-        case '/valliscycle':
+        case '!!valliscycle':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['vallisCycle']
-        case '/午夜电波':
+        case '!!午夜电波':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['nightwave']
-        case '/电波':
+        case '!!电波':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['nightwave']
-        case '/nightwave':
+        case '!!nightwave':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['nightwave']
-        case '/仲裁':
+        case '!!仲裁':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['arbitration']
-        case '/arbitration':
+        case '!!arbitration':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['arbitration']
-        case '/火卫二时间':
+        case '!!火卫二时间':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cambionCycle']
-        case '/cambioncycle':
+        case '!!cambioncycle':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['cambionCycle']
-        case '/扎里曼':
+        case '!!扎里曼':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['zarimanCycle']
-        case '/扎里曼派系':
+        case '!!扎里曼派系':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['zarimanCycle']
-        case '/zarimancycle':
+        case '!!zarimancycle':
             url += config['api']['warframe']
             url += config['api']['warframe-path']['zarimanCycle']
         case default:
             noParse = True
     if noParse:
-        if raw_message.startswith('/市场') or raw_message.startswith('/market'):
-            parsed = parse('/市场 {item}', raw_message)
+        if raw_message.startswith('!!市场') or raw_message.startswith('!!market'):
+            parsed = parse('!!市场 {item}', raw_message)
             url += config['api']['market']
             if type(parsed) == type(None):
-                parsed = parse('/market {item}', raw_message)
+                parsed = parse('!!market {item}', raw_message)
             url += parsed['item']
             return url
-        elif raw_message.startswith('/紫卡') or raw_message.startswith('/riven'):
-            parsed = parse('/紫卡 {item}', raw_message)
+        elif raw_message.startswith('!!紫卡') or raw_message.startswith('!!riven'):
+            parsed = parse('!!紫卡 {item}', raw_message)
             url += config['api']['riven']
             if type(parsed) == type(None):
-                parsed = parse('/riven {item}', raw_message)
+                parsed = parse('!!riven {item}', raw_message)
             url += parsed['item']
             return url
         else:
